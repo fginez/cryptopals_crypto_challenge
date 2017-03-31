62e94bf92e1592520170b4d65d4add1db470ef49
Fernando Ginez da Silva <fginez@gmail.com>
2017-03-31 19:51:53 -0300

    Fixed general issues

--- src/tests/s2c12_ecb_byteattime_break.c
+++ src/tests/s2c12_ecb_byteattime_break.c
@@ -5,7 +5,9 @@
 #include "../include/encryption_oracle.h"
 #include "../include/pkcs7padding.h"
 
-unsigned char base_chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/ .,;´`'\"?!";
+//#define __C12_DUMP_DICTIONARY__
+
+unsigned char base_chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789`~!@#$%^&()-=_+[]{}/\\|;:'\",./<>? \t\r\n";
 unsigned char dictionary[sizeof(base_chars)][BLOCK_LEN];
 unsigned char encoded_input[] = "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK";
 
@@ -14,36 +16,59 @@ char* s2c12_getdesc()
 	return "Challenge 12 Byte-at-a-time ECB decryption (Simple)";
 }
 
-char get_char_from_dictionary(unsigned char* cryptogram)
+char get_char_from_dictionary(unsigned char* cryptogram, int len)
 {
-	int len = sizeof(base_chars);
+	int baselen = sizeof(base_chars);
 	int i;
 
-	for(i=0; i<len; i++) {
+	if(BLOCK_LEN != len) {
+		printf ("Invalid ciphertext length -- len=%d\n", len);
+		return '*';
+	}
+
+	for(i=0; i<baselen; i++) {
 		if (0 == CRYPTO_memcmp(cryptogram, dictionary[i], BLOCK_LEN) ) {
+#ifdef _DEBUG
+			printf("\tinput ");
+			print_hex(cryptogram, BLOCK_LEN);
+			printf(" matches dic. entry ");
+			print_hex(dictionary[i], BLOCK_LEN);
+			printf(" mapped to index %d char '%c'\n", i, base_chars[i]);
+#endif
 			return base_chars[i];
 		}
 	}
 
-	return ' ';
+	return '*';
 }
 
 int s2c12_main(int argc, char** argv)
 {
 	int i, outlen, decoded_in_len, ciphertext_len;
-	unsigned char input[16], ciphertext[16];
+	unsigned char input[BLOCK_LEN], ciphertext[BLOCK_LEN];
 	unsigned char* decoded_in;
-
 
 	// Build dictionary
 	for(i=0; i<sizeof(base_chars); i++) {
-		memset(input, 0, sizeof(input));
+		memset(input, 0x00, BLOCK_LEN);
 		input[BLOCK_LEN-1] = base_chars[i];
-		if (0 != aes128ecb_oracle(input, BLOCK_LEN, 
-								  dictionary[i], (unsigned int*) &outlen, BLOCK_LEN) ) {
+		if (0 != aes128ecb_oracle(input, BLOCK_LEN, dictionary[i], (unsigned int*) &outlen, BLOCK_LEN) ) {
+			return -1;
+		}
+
+		if ( BLOCK_LEN != outlen ){
+			printf("Failed to build dictionary -- outlen=%d", outlen);
 			return -1;
 		}
 	}
+
+#ifdef __C12_DUMP_DICTIONARY__
+	for(i=0;i<sizeof(base_chars);i++){
+		printf("dic. entry for '%c': ", base_chars[i]);
+		print_hex(dictionary[i], BLOCK_LEN);
+		printf("\n");
+	}
+#endif
 
 	decoded_in_len = b64tohex_sizehelper(sizeof(encoded_input));
 	decoded_in = (unsigned char*) malloc(decoded_in_len);
@@ -55,15 +80,25 @@ int s2c12_main(int argc, char** argv)
 	decoded_in_len = b64tohex(encoded_input, strlen((char*)encoded_input), decoded_in);
 	printf("Breaking cryptogram:\n");
 	for(i=0; i<decoded_in_len;i++) {
-		memset(input, 0, sizeof(input));
-		input[BLOCK_LEN-1] = decoded_in[i];	
+		memset(input, 0x00, BLOCK_LEN);
+		input[BLOCK_LEN-1] = decoded_in[i];
 
 		// Encrypt it
 		if ( 0 != aes128ecb_oracle(input, BLOCK_LEN, ciphertext, (unsigned int*) &ciphertext_len, BLOCK_LEN) ) {
 			return -1;
 		}
 
-		printf("%c", get_char_from_dictionary(ciphertext));
+		if ( BLOCK_LEN != ciphertext_len ) {
+			printf("Unexpected ciphertext_len -- len=%d\n", ciphertext_len);
+			return -1;
+		}
+
+		printf("%c", get_char_from_dictionary(ciphertext, ciphertext_len));
+#ifdef _DEBUG
+		printf(" - ");
+		print_hex(ciphertext, ciphertext_len);
+		printf("\n");
+#endif
 	}
 
 	return 0;
