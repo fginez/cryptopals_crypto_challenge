5383461790c74186e39f6189a394e0b2485dcf20
Fernando Ginez da Silva <fginez@gmail.com>
2017-03-31 19:15:59 -0300

    Check for printable chars before putting it into console

--- src/util/printbuffer.c
+++ src/util/printbuffer.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
 #include "printbuffer.h"
 
+char is_printable_char(const unsigned char c)
+{
+  if ( c >= 0x20 || c==0x0a || c==0x0d ) {
+    return 1;
+  }
+
+  return 0;
+}
+
 void print_chars(const unsigned char *in, const unsigned int len)
 {
     int i;
-    for (i=0; (unsigned int)i<len; i++)
-        printf("%c", in[i]);
+    for (i=0; (unsigned int)i<len; i++) {
+        if (is_printable_char(in[i])){
+          printf("%c", in[i]);
+        }
+        else {
+          printf("[**Non-printable:0x%2.2x**]", in[i]);
+        }
+    }
 }
 
 void print_hex(const unsigned char *in, const unsigned int len)
