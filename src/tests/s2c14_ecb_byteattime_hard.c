a1db1ba4b3603b90b9979f2de55bdcdb2f435478
Fernando Ginez da Silva <fginez@gmail.com>
2017-03-31 19:52:27 -0300

    Source created

---
+++ src/tests/s2c14_ecb_byteattime_hard.c
@@ -0,0 +1,12 @@
+#include "../include/cryptopals.h"
+#include "../include/libutil.h"
+
+char* s2c14_getdesc()
+{
+	return "Challenge 14 Byte-at-a-time ECB decryption (Hard)";
+}
+
+int s2c14_main(int argc, char** argv)
+{
+  return 0;
+}
